4518ca993419ba395de2e79d5cdf1506b348f9cd
Kaiowa15 <93450381+Kaiowa15@users.noreply.github.com>
2021-11-09 21:38:57 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+var player, player_running;
+
+var path, path_image;
+
+function preload(){
+  //imagens pré-carregadas
+  player_running = loadAnimation("Runner-1.png", "Runner-2.png")
+
+ path_image = loadImage("path.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  //crie sprite aqui
+
+ player = createSprite(180,360,10,10)
+  
+ player.addAnimation("running", player_running);
+ player.scale = 0.05
+
+ path = createSprite(200,200,400,280)
+ 
+ path.addImage("path",path_image)
+
+
+
+}
+
+function draw() {
+  background(55);
+
+
+
+
+
+
+
+
+
+
+
+
+ drawSprites ()
+
+}
